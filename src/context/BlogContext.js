5937e704132a70351d5f71ebb9a4a82b4dc78daa
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-11-20 08:16:07 -0800

    133: Adding State in Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext(); //think of this as a direct line pipe to bypass props.
 
@@ -7,13 +7,20 @@ const BlogContext = React.createContext(); //think of this as a direct line pipe
 // this is called a 'named export' because it's not using `export default`: export const BlogProvider = ({ children}) => {
 
   export const BlogProvider = ({ children }) => {
+    const [blogPosts, setterBlogPosts] = useState([])
+
+
+    const addBlogPost = () => {
+      setterBlogPosts([...blogPosts], { title: `Blog Post #${blogPosts.length + 1}`}) //create a brand new array from scratch, don't change existing 'blogPosts', and use ... to add in current data. Then add the new in the object
+    }
+
     //Context 5: add value prop
     const blogPosts = [
       { title: 'Post 1'},
       { title: 'Post 2'},
     ]
-
-    return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>;
+    // pass in object with data and addBlogPost callback function 
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost}}>{children}</BlogContext.Provider>; // key and value are indentical for addBlogPost, so we could condense to one value like { data: blogPosts, addBlogPost}
   };
 
   export default BlogContext;
