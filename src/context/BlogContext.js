cf8cd9529a8c31bf63f27814f47b2b080c73c291
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-11-20 13:10:49 -0800

    136: Updating with UseReducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,4 +1,17 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react'; //useReducer 1
+// TOO COMPLEX FOR NUMBERED NOTES!
+// But, Stephen Grider has an excellect step by step explanation in 136: Updating with UseReducter (at 6:52)
+// see https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15707480#questions
+
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost': 
+      return [...state, {title: `Blog Post #${state.length+1}`}];
+    default: 
+      return state;
+  }
+}
 
 const BlogContext = React.createContext(); //think of this as a direct line pipe to bypass props.
 
@@ -7,18 +20,14 @@ const BlogContext = React.createContext(); //think of this as a direct line pipe
 // this is called a 'named export' because it's not using `export default`: export const BlogProvider = ({ children}) => {
 
   export const BlogProvider = ({ children }) => {
-    const [blogPosts, setterBlogPosts] = useState([])
+    const [blogPosts, dispatch] = useReducer(blogReducer, []) //first arg in useReducers is reducer to use, second is the intitial state object
 
-    //Context 5: add value prop and callback prop
     const addBlogPost = () => {
-      setterBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}]) //create a brand new array from scratch, don't change existing 'blogPosts', and use ... to add in current data. Then add the new in the object
+      dispatch({type: 'add_blogpost'});
     }
-    // const blogPosts = [
-    //   { title: 'Post 1'},
-    //   { title: 'Post 2'},
-    // ]
+
     // pass in object with data and addBlogPost callback function 
-    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost}}>{children}</BlogContext.Provider>; // key and value are indentical for addBlogPost, so we could condense to one value like { data: blogPosts, addBlogPost}
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>{children}</BlogContext.Provider>; 
   };
 
   export default BlogContext;
