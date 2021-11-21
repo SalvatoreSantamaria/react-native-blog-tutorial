1c40ba41273f2a6eca246c64c8a0aa904c6190b8
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-11-20 19:56:41 -0800

    139: A Bit of Styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'; //Context 3: import the useContext hook from react
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext'; //Context 4: import the context function
+import { Feather } from '@expo/vector-icons';
 
 
 const IndexScreen = () => {
@@ -16,13 +17,35 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({item}) => {
-          return <Text>{item.title}</Text>
+          return <View style={styles.row}>
+            <Text style={styles.title}>{item.title}
+              <Feather name='trash' style={styles.icon}/>
+            </Text>
+          </View>
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',//show on same line
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10, 
+    borderTopWidth: 1, 
+    borderColor: 'gray'
+
+  }, 
+  title: {
+    fontSize: 18
+  }, 
+  icon: {
+    fontSize: 24
+  }
+
+
+});
 
 export default IndexScreen;
