7804c863e8beaf88a3c8fc374241e5b014654ec6
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-11-21 06:33:03 -0800

    143: Retriving Single Posts

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
-const ShowScreen = () => {
+const ShowScreen = ({navigation}) => {
+  console.log(navigation.getParam('id'))
+  const { state } = useContext(Context) //state comes from the value property in createDataContext which passes in 'state' as a property
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+
   return (
     <View>
-      <Text>Index Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 };
