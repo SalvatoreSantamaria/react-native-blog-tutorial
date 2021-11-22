64b0a2bee4e2a3c71fa7d863f660766029bb9540
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-11-21 20:03:16 -0800

    151: The Edit Icon Link

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons'
 
 const ShowScreen = ({navigation}) => {
   console.log(navigation.getParam('id'))
   const { state } = useContext(Context) //state comes from the value property in createDataContext which passes in 'state' as a property
   const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
-
+  console.log('this is blogPost ', blogPost)
   return (
     <View>
       <Text>{blogPost.title}</Text>
@@ -14,6 +15,16 @@ const ShowScreen = ({navigation}) => {
     </View>
   );
 };
+
+ShowScreen.navigationOptions = () => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
+}
 
 const styles = StyleSheet.create({});
 
